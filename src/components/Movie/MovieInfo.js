a5cd1c66cec5e0e96fa13d515a01c638ea531a85
ri-barcaza <ri.barcaza@outlook.com>
2022-06-20 20:58:16 -0400

    modal movie

--- src/components/Movie/MovieInfo.js
+++ src/components/Movie/MovieInfo.js
@@ -1,12 +1,50 @@
-import React from "react";
-import { VideoCameraOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import { PlayCircleOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import moment from "moment";
+import useFetch from "../../hooks/useFetch";
+import { API_URL, API_KEY } from "../../utils/constants";
+import ModalVideo from "../ModalVideo/ModalVideo";
 
 export default function MovieInfo(props) {
   const {
     movieDetails: { id, title, release_date, overview, genres },
   } = props;
+
+  const videoMovie = useFetch(
+    `${API_URL}/movie/${id}/videos?api_key=${API_KEY}&language=es-ES`
+  );
+
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+
+  const openModal = () => setIsVisibleModal(true);
+  const closeModal = () => setIsVisibleModal(false);
+
+  const renderVideo = () => {
+    if (videoMovie.result) {
+      if (videoMovie.result.results.length > 0) {
+        return (
+          <>
+            <Button
+              type="primary"
+              icon={<PlayCircleOutlined />}
+              size="large"
+              onClick={openModal}
+            >
+              VER TRAILER
+            </Button>
+
+            <ModalVideo
+              videoKey={videoMovie.result.results[0].key}
+              videoPlatform={videoMovie.result.results[0].site}
+              isOpen={isVisibleModal}
+              close={closeModal}
+            />
+          </>
+        );
+      }
+    }
+  };
 
   return (
     <>
@@ -26,11 +64,7 @@ export default function MovieInfo(props) {
         </ul>
       </div>
 
-      <div className="movie__info-trailer">
-        <Button type="primary" icon={<VideoCameraOutlined />} size="large">
-          VER TRAILER
-        </Button>
-      </div>
+      <div className="movie__info-trailer">{renderVideo()}</div>
     </>
   );
 }
